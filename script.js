2c128c3b4a6d43ca00c65a19ee377facb9b13c96
byronn28 <90810319+byronn28@users.noreply.github.com>
2026-07-14 17:40:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Página cargada correctamente ❤️");
+const button = document.getElementById("enterButton");
+
+const welcome = document.getElementById("welcome");
+
+const content = document.getElementById("content");
+
+button.onclick = () => {
+
+    welcome.style.opacity = "0";
+
+    setTimeout(() => {
+
+        welcome.style.display = "none";
+
+    },1000);
+
+    content.style.opacity = "1";
+
+    content.style.transform = "translateY(0)";
+
+}
